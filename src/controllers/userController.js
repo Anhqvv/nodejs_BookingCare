ee92cbfcf2442c779e815ec0385e10669ed244f7
Anhqvv <anhqvv2022@gmail.com>
2022-10-07 21:46:27 +0700

    Nodejs #37.2 change query

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,50 +1,49 @@
-import userService from '../services/userService';
+import userService from "../services/userService";
 
 let handleLoging = async (req, res) => {
-    let email = req.body.email;
-    let password = req.body.password;
+   let email = req.body.email;
+   let password = req.body.password;
 
-    if (!email || !password) {
-        return res.status(500).json({
-            errCode: 1,
-            message: 'Missing inputs parameter!'
-        })
-    }
+   if (!email || !password) {
+      return res.status(500).json({
+         errCode: 1,
+         message: "Missing inputs parameter!",
+      });
+   }
 
-    let userData = await userService.handleUserLogin(email, password)
-    //check email exist
-    //password nhap vao ko dung
-    //return userInfor
-    // access_token :JWT json web token
+   let userData = await userService.handleUserLogin(email, password);
+   //check email exist
+   //password nhap vao ko dung
+   //return userInfor
+   // access_token :JWT json web token
 
-    return res.status(200).json({
-        errCode: userData.errCode,
-        message: userData.errMessage,
-        user: userData.user ? userData.user : {}
-    })
-}
+   return res.status(200).json({
+      errCode: userData.errCode,
+      message: userData.errMessage,
+      user: userData.user ? userData.user : {},
+   });
+};
 
-let handleGetAllUsers = async(req, res) => {
-    let id = req.body.id
-    if(!id ){
-        return res.status(500).json({
-            errCode: 1,
-            errMessage:"Missing required parameters",
-            user: []
-        })
-    }
-    // console.log('handleGetAllUsers id',id)
-    let users = await userService.getAllUsers(id)
-    return res.status(200).json( {
-        errCode: 0,
-        errMessage: "OK",
-        users
-    })
-
-}
-
+let handleGetAllUsers = async (req, res) => {
+   let id = req.query.id
+   console.log("Nodejss handleGetAllUsers id: ", id);
+   if (!id) {
+      return res.status(500).json({
+         errCode: 1,
+         errMessage: "Missing required parameters",
+         user: [],
+      });
+   }
+   // console.log('handleGetAllUsers id',id)
+   let users = await userService.getAllUsers(id);
+   return res.status(200).json({
+      errCode: 0,
+      errMessage: "OK",
+      users,
+   });
+};
 
 module.exports = {
-    handleLoging,
-    handleGetAllUsers,
-}
+   handleLoging,
+   handleGetAllUsers,
+};
